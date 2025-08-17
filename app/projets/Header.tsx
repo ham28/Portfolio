10226b666e08c6350441558a5f3764679c6f1dcb
Hasina-ades <hasindraibe.randriatsarafara@ades-solaire.org>
2025-08-17 20:27:10 +0300

    debuge

--- app/projets/Header.tsx
+++ app/projets/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="bg-gray-800 text-gray-200 p-6">
@@ -5,9 +7,9 @@ export const Header = () => {
         <h1 className="text-2xl font-bold">Portfolio</h1>
         <nav>
           <ul className="flex space-x-4">
-            <li><a href="/" className="hover:text-gray-400">Home</a></li>
-            <li><a href="/projets" className="hover:text-gray-400">Projets</a></li>
-            <li><a href="/contact" className="hover:text-gray-400">Contact</a></li>
+            <li><Link href="/" className="hover:text-gray-400">Home</Link></li>
+            <li><Link href="/projets" className="hover:text-gray-400">Projets</Link></li>
+            <li><Link href="/contact" className="hover:text-gray-400">Contact</Link></li>
           </ul>
         </nav>
       </div>
