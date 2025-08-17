2819a98709e280a636be8436ffa3b4b43cd6faa4
Hasina-ades <hasindraibe.randriatsarafara@ades-solaire.org>
2025-08-17 20:21:09 +0300

    debuge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Anek_Telugu } from "next/font/google";
+import { Geist, Anek_Telugu } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
